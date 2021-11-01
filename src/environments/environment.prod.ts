9d93f516e35f37fe20731570c13c03c4892ff741
Aziz Çetin <cetinaz16@itu.edu.tr>
2021-11-01 21:02:05 +0300

    Environment prod is edited.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Dev Environment Heading',
-  weatherApi: 'http://api.openweathermap.org',
+  weatherApi: 'https://api.openweathermap.org',
   weatherApiKey:'d3f7cdafe71f25006301071f0ebee61a'
 };
