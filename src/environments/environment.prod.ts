56f37dbb850d42063ea283fe4b5d48b4f3dcf142
Aziz Çetin <cetinaz16@itu.edu.tr>
2021-11-01 20:52:08 +0300

    Enviremont is edited.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  title: 'Dev Environment Heading',
+  weatherApi: 'http://api.openweathermap.org',
+  weatherApiKey:'d3f7cdafe71f25006301071f0ebee61a'
 };
